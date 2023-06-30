56e2f9251eb0f4cecc8787f171c4ef74b24ac1ab
PolyanskayaP <ppolina353@yandex.ru>
2023-06-30 23:09:41 +0300

    first commit

---
+++ page.js
@@ -0,0 +1,21 @@
+var a = 10;
+var b = 20;
+
+var c = a + b;
+alert(c);
+console.log("Сумма a+b="+c); //в консоль
+
+document.getElementById("d1").innerHTML = c;
+
+document.write("<p>Контент</p>");
+document.write("<button>Наша кнопка</button><br>");
+
+for (i=0; i<10; i++) {
+    document.write("<div> :) "+ i + "</div>")
+}
+
+document.write("<br>");
+
+for (i=0; i<10; i++) {
+    document.write(" ;) "+ i + "</br>")
+}
