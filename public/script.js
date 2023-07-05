53553ed9c18c2d1f42d56d5f9e0bca13d83c62dc
PolyanskayaP <ppolina353@yandex.ru>
2023-07-05 17:55:12 +0300

    вход выход

--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
+var flagVhod = false; 
+document.getElementById("but_dis").style.backgroundColor = "#c0c0c0"; 
 
 function myFunction() {
     document.getElementById("exampleBut").innerHTML = "!";
@@ -6,6 +8,31 @@ function myFunction() {
 
 function DisabledButton() {
     document.getElementById("but_dis").disabled = ""; 
+}
+
+function textVhod() {
+    if (flagVhod==false) {
+        document.getElementById("vhodButton").innerText = "Выйти";
+        flagVhod = true;
+        document.getElementById("but_dis").disabled = ""; 
+        document.getElementById("but_dis").style.backgroundColor = "#8A2BE2"; 
+        document.getElementById("messageKupit").innerText = "";
+    }
+}
+
+function zapretVhod() {
+    if (flagVhod) {
+        //запретить показ Modal Vhod
+        //$('#staticBackdropVhod').modal('hide');
+        //document.getElementById("closeModalVhod").click();
+
+        //поменять Выйти на Вход
+        document.getElementById("vhodButton").innerText = "Вход";
+        flagVhod = false; 
+        document.getElementById("but_dis").disabled = "disabled"; 
+        document.getElementById("but_dis").style.backgroundColor = "#c0c0c0"; 
+        document.getElementById("messageKupit").innerText = "Войдите, чтобы купить";
+    }
 }
 
 //list-b-list
